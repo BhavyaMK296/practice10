1ac4be5410975621049050a9842e97101bbdbae9
BhavyaMK296 <bhavyakanthraj@gmail.com>
2025-06-03 07:10:25 +0530

    Initial commit

---
+++ practice10/src/main/java/BhavyaMK296/App.java
@@ -0,0 +1,13 @@
+package BhavyaMK296;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
